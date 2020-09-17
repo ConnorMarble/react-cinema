d601f2e4b959eaadfa2347939362ad62f17a7853
Connor Marble <connor.marble@gmail.com>
2020-09-17 13:11:11 -0400

    add rating component

--- src/components/content/main-content/MainContent.js
+++ src/components/content/main-content/MainContent.js
@@ -1,23 +1,105 @@
 import React, { useState } from "react";
-import Paginate from "../paginate/Paginate";
+
 import "./MainContent.scss";
 import Slideshow from "./slide-show/Slideshow";
+import Paginate from "../paginate/Paginate";
+import Grid from "../grid/Grid";
+
 const MainContent = () => {
   const images = [
     {
       url:
         "https://images.pexels.com/photos/688574/pexels-photo-688574.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      rating: 5.5,
+    },
+    {
+      url:
+        "https://images.pexels.com/photos/688574/pexels-photo-688574.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      rating: 8.5,
     },
     {
       url:
         "https://images.pexels.com/photos/776653/pexels-photo-776653.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      rating: 7.8,
+    },
+    {
+      url:
+        "https://images.pexels.com/photos/776653/pexels-photo-776653.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      rating: 9.7,
+    },
+    {
+      url:
+        "https://images.pexels.com/photos/776653/pexels-photo-776653.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      rating: 6.5,
     },
     {
       url:
         "https://images.pexels.com/photos/255379/pexels-photo-255379.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      rating: 8.5,
+    },
+    {
+      url:
+        "https://images.pexels.com/photos/688574/pexels-photo-688574.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      rating: 5.5,
+    },
+    {
+      url:
+        "https://images.pexels.com/photos/688574/pexels-photo-688574.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      rating: 8.5,
+    },
+    {
+      url:
+        "https://images.pexels.com/photos/776653/pexels-photo-776653.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      rating: 7.8,
+    },
+    {
+      url:
+        "https://images.pexels.com/photos/776653/pexels-photo-776653.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      rating: 9.7,
+    },
+    {
+      url:
+        "https://images.pexels.com/photos/776653/pexels-photo-776653.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      rating: 6.5,
+    },
+    {
+      url:
+        "https://images.pexels.com/photos/255379/pexels-photo-255379.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      rating: 8.5,
+    },
+    {
+      url:
+        "https://images.pexels.com/photos/688574/pexels-photo-688574.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      rating: 5.5,
+    },
+    {
+      url:
+        "https://images.pexels.com/photos/688574/pexels-photo-688574.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      rating: 8.5,
+    },
+    {
+      url:
+        "https://images.pexels.com/photos/776653/pexels-photo-776653.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      rating: 7.8,
+    },
+    {
+      url:
+        "https://images.pexels.com/photos/776653/pexels-photo-776653.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      rating: 9.7,
+    },
+    {
+      url:
+        "https://images.pexels.com/photos/776653/pexels-photo-776653.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      rating: 6.5,
+    },
+    {
+      url:
+        "https://images.pexels.com/photos/255379/pexels-photo-255379.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      rating: 8.5,
     },
   ];
   const [currentPage, setCurrentPage] = useState(1);
+
   const paginate = (type) => {
     if (type === "prev" && currentPage >= 1) {
       setCurrentPage((prev) => prev - 1);
@@ -25,6 +107,7 @@ const MainContent = () => {
       setCurrentPage((prev) => prev + 1);
     }
   };
+
   return (
     <div className="main-content">
       <Slideshow images={images} auto={true} showArrows={true} />
@@ -35,10 +118,10 @@ const MainContent = () => {
             currentPage={currentPage}
             totalPages={10}
             paginate={paginate}
-          ></Paginate>
+          />
         </div>
       </div>
-      {/* display grid component */}
+      <Grid images={images} />
     </div>
   );
 };
